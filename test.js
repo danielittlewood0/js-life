4500acc2c0f1c1277556ba45b5e07549a25a1ec9
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-22 08:16:13 +0100

    simple init test passing

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 import Board from './board.js'
 const assert = require('assert')
 
-it('should return true', () => {
-  var board = new Board();
-  assert.equal(true,true)
+it('should initialise with a board if given (height,width)', () => {
+  var board = new Board(2,3);
+  assert.deepEqual([],[])
+  assert.deepEqual(board.board,[[0,0,0],[0,0,0]])
 })
