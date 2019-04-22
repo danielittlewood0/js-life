76f92b4cf1c14867d701d9a552fdf0dfc3b516c0
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-22 08:56:26 +0100

    board is local state of component

--- src/LifeBoard.js
+++ src/LifeBoard.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Board from './Board.js';
 
 class LifeBoard extends Component{
+  constructor(){
+    super();
+    this.state = {
+      board: []
+    }
+  }
+
   render(){
     return( 
       <p> bing </p>
