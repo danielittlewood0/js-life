408e5e31b655a4187fb5b64542a3e44b72a90267
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-22 06:39:11 +0100

    bad practice, wrote this out of git

---
+++ app.js
@@ -0,0 +1,92 @@
+class Board{
+  constructor(height,width){
+    this.width = width
+    this.height = height
+    this.clearBoard();
+  }
+
+  clearBoard(){
+    this.board = Array.from(Array(this.height),() => 
+      Array.from(Array(this.width),() => 
+        0))
+  }
+
+  flip(i,j){
+    this.board[i][j] = 1 - this.board[i][j]
+  }
+  makeDead(i,j){
+    this.board[i][j] = 0
+  }
+  makeAlive(i,j){
+    this.board[i][j] = 1
+  }
+
+  inBoard(i,j){
+    return ( 
+      i >= 0 && 
+      i <  this.height && 
+      j >= 0 && 
+      j < this.width
+    )
+  }
+
+  neighbours(i,j){
+    var nbrs = []
+    for(var a=-1; a <= 1; a++){
+      var row = []
+      for(var b=-1; b <= 1; b++){
+        if(a==0 && b==0){ }
+        else if( this.inBoard(i+a,j+b) ){ 
+          row.push(this.board[i+a][j+b]) }
+        else{ }
+      }
+      nbrs.push(row)
+    }
+    return nbrs;
+  }
+  aliveNbrs(i,j){
+    var nbrs = this.neighbours(i,j);
+    return [].concat(...nbrs).filter( x => x==1 ).length;
+  }
+
+  advance(){
+    var newBoard = []
+    for(var i=0; i < this.height; i++){
+      var row = []
+      for(var j=0; j < this.width; j++){ row.push(this.advanceSquare(i,j)) }
+      newBoard.push(row)
+    }
+    this.board = newBoard;
+    return this;
+  }
+
+  advanceSquare(i,j){
+    var alives = this.aliveNbrs(i,j);
+    if (alives < 2 || alives > 3){ return 0 }
+    else if (alives == 2 && (this.board[i,j] == 0)){ return 0 }
+    else { 
+      var msg = i + " " + j
+      console.log(msg);
+      return 1 }
+  }
+}
+var board = new Board(8,8);
+board.board = [
+  [0,1,0,0,0,0,0,0],
+  [0,0,1,0,0,0,0,0],
+  [1,1,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0]
+]
+console.log(board);
+setInterval( () => {
+  board.advance();
+  console.log(board);
+}, 1000)
+//console.log(board.inBoard(0,1))
+//console.log(board.neighbours(0,0))
+//console.log(board.aliveNbrs(0,0))
+//console.log(board.advance())
